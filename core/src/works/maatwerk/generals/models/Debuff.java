a10d76c76788883727d69dcf7a28550cf71a3905
REXOTIUM <rick.pie@hotmail.com>
2018-03-06 00:35:41 +0100

    Update Debuff.java

--- core/src/works/maatwerk/generals/models/Debuff.java
+++ core/src/works/maatwerk/generals/models/Debuff.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package works.maatwerk.generals.models;
 
 /**
@@ -13,6 +8,12 @@ public class Debuff {
     private int turns;
     private Stats staticDebuff;     //wat hetzelfde blijft
     private Stats dynamicDebuff;    //wat optelt
+    
+    public Debuff(int turns, Stats staticDebuff, Stats dynamicDebuff) {
+        this.turns = turns;
+        this.staticDebuff = staticDebuff;
+        this.dynamicDebuff = dynamicDebuff;
+    }
     
     public int getTurns() {
         return turns;
@@ -35,12 +36,6 @@ public class Debuff {
     }
 
     public void setDynamicDebuff(Stats dynamicDebuff) {
-        this.dynamicDebuff = dynamicDebuff;
-    }
-
-    public Debuff(int turns, Stats staticDebuff, Stats dynamicDebuff) {
-        this.turns = turns;
-        this.staticDebuff = staticDebuff;
         this.dynamicDebuff = dynamicDebuff;
     }
 
