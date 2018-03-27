4a9c297a92e37913083f40662b5bb8650a909fb3
Alexander Vegt <alexander@contritio.com>
2018-03-27 04:23:30 +0200

    *shruggles*

--- core/src/works/maatwerk/generals/utils/files/Paths.java
+++ core/src/works/maatwerk/generals/utils/files/Paths.java
@@ -1,5 +1,8 @@
 package works.maatwerk.generals.utils.files;
 
 public class Paths {
+    private Paths() {
+    }
+
     public static final String MUSIC_FOLDER = "data/music";
 }
--- core/src/works/maatwerk/generals/utils/logger/Tag.java
+++ core/src/works/maatwerk/generals/utils/logger/Tag.java
@@ -1,6 +1,9 @@
 package works.maatwerk.generals.utils.logger;
 
 public class Tag {
+    private Tag() {
+    }
+    
     public static final String MUSIC = "Music";
     public static final String NETWORKING = "Networking";
 
