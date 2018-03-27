f05ea9229629b73e6bbbdca841213755280c6520
nodenba <nodenba@hotmail.com>
2018-03-27 11:18:25 +0200

    Map donkerder gemmakt

--- core/src/works/maatwerk/generals/LoadingScreen.java
+++ core/src/works/maatwerk/generals/LoadingScreen.java
@@ -14,9 +14,9 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
-import works.maatwerk.generals.utils.files.*;
+import works.maatwerk.generals.utils.files.Paths;
 
 
 @SuppressWarnings("SpellCheckingInspection")
