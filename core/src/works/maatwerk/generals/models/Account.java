207f0f2923360b28c9540243e8e416169cea03c3
joeyboy108 <teun.de.wijs@live.nl>
2018-03-27 12:53:32 +0200

    improved code for code climate

--- core/src/works/maatwerk/generals/models/Account.java
+++ core/src/works/maatwerk/generals/models/Account.java
@@ -9,7 +9,6 @@ import java.security.NoSuchAlgorithmException;
 public class Account {
     private String Username;
     private String Password;
-    private static final char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
     public String getUsername() {
         return Username;
@@ -21,25 +20,7 @@ public class Account {
 
     public Account(String username, String password) {
         Username = username;
-
-            Password = password;
-
-        System.out.println(this.toString());
-    }
-
-    public static String byteArray2Hex(byte[] bytes) {
-        StringBuffer sb = new StringBuffer(bytes.length * 2);
-        for(final byte b : bytes) {
-            sb.append(hex[(b & 0xF0) >> 4]);
-            sb.append(hex[b & 0x0F]);
-        }
-        return sb.toString();
-    }
-
-    public static String getStringFromSHA256(String stringToEncrypt) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-        messageDigest.update(stringToEncrypt.getBytes());
-        return byteArray2Hex(messageDigest.digest());
+        Password = password;
     }
 
     @Override
