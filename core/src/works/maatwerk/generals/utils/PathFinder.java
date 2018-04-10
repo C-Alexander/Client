12e8faac3ff04e651696442590fb6e0b2f5ad0a7
REXOTIUM <rick.pie@hotmail.com>
2018-04-10 15:26:30 +0200

    test 7

--- core/src/works/maatwerk/generals/utils/PathFinder.java
+++ core/src/works/maatwerk/generals/utils/PathFinder.java
@@ -53,8 +53,9 @@ public class PathFinder {
         }*/
 	//boolean improve = output[v.getX()][v.getY()] == null ? true : output[v.getX()][v.getY()] < (moves - tiles[v.getX()][v.getY()]);
         //int used = movesUsed + (start ? 0 : tiles[v.getX()][v.getY()]);
+        boolean improve = true;
         int used = movesUsed + tiles[v.getX()][v.getY()];
-	if(tiles[v.getX()][v.getY()] > 0 && tiles[v.getX()][v.getY()] <= moves /*&& improve*/) {
+	if(tiles[v.getX()][v.getY()] > 0 && tiles[v.getX()][v.getY()] <= moves && improve) {
             output[v.getX()][v.getY()] = moves - tiles[v.getX()][v.getY()];
             output = genIntMap(movement, tiles, character, v.addOneX(), output, used, false);
             output = genIntMap(movement, tiles, character, v.subOneX(), output, used, false);
